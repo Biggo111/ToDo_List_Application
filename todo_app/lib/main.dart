eea2660a779c45b518635309d7680cf80ab70789
Biggo Routh <biggo@Biggos-MacBook-Air.local>
2022-08-19 16:09:56 +0600

    Edited taskpage

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Todo application',
-      home: TaskPage(),
+      home: HomePage(),
     );
   }
 }
--- todo_app/lib/screens/taskpage.dart
+++ todo_app/lib/screens/taskpage.dart
@@ -11,23 +11,48 @@ class TaskPage extends StatelessWidget {
           //color: Colors.red,
           child: Column(
             children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: Image(
-                      image: AssetImage('assets/icon_images/arrow.png'),
-                      height: 20.0,
-                    ),
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Enter the task',
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 24.0,
+                  bottom: 6.0,
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Image(
+                          image: AssetImage('assets/icon_images/arrow.png'),
+                          height: 20.0,
+                        ),
                       ),
                     ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter the task',
+                        ),
+                        style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: '->Enter the task description here...',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 50.0,
+                    vertical: 50.0,
                   ),
-                ],
+                ),
               ),
             ],
           ),
